455cd05587a46902c7976f7371e08eb3065d3272
Lachlan Heywood <lachieh@users.noreply.github.com>
2022-01-18 22:00:35 +0000

    add solution for day 1.1

---
+++ day1_1/index.ts
@@ -0,0 +1,13 @@
+const day1_1 = (input: string) => {
+  // solve
+  const inputArray = input.split('\n')
+  let count = 0
+  for (let i = 1; i < inputArray.length; i++) {
+    const current = Number(inputArray[i])
+    const previous = Number(inputArray[i - 1])
+    if (current > previous) {count++}
+  }
+  return count
+}
+
+export default day1_1
