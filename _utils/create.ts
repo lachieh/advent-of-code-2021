93daa40f30247e166c1e7281960591ed48b6461b
Lachlan Heywood <lachieh@users.noreply.github.com>
2022-01-21 16:10:53 +0000

    fix path on create

--- _utils/create.ts
+++ _utils/create.ts
@@ -1,22 +1,23 @@
 import { existsSync, mkdirSync, writeFileSync } from 'fs'
 import { join } from 'path';
 
-const newFolder = process.argv[2]
+const name = process.argv[2]
+const newFolder = '../' + name
 if (!newFolder) throw new Error('include path name')
 
 const folderExists = existsSync(join(__dirname, newFolder))
 if (folderExists) throw new Error('folder already exists')
 
-const newFile = join(__dirname, '../', newFolder, 'index.ts')
-const newInput = join(__dirname, '../', newFolder, 'input.txt')
+const newFile = join(__dirname, newFolder, 'index.ts')
+const newInput = join(__dirname, newFolder, 'input.txt')
 
 mkdirSync(join(__dirname, newFolder))
-writeFileSync(newFile, `// Advent of Code: ${newFolder}
-const ${newFolder} = (input: string) => {
+writeFileSync(newFile, `// Advent of Code: ${name}
+const ${name} = (input: string) => {
   // solve
 }
 
-export default ${newFolder}
+export default ${name}
 `)
 writeFileSync(newInput, '')
 
