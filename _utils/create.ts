798f365f9dea4073fa30a31ed96c28cf8fc2bed1
Lachlan Heywood <lachieh@users.noreply.github.com>
2022-01-21 17:00:03 +0000

    add prettier

--- _utils/create.ts
+++ _utils/create.ts
@@ -1,18 +1,20 @@
-import { existsSync, mkdirSync, writeFileSync } from 'fs'
-import { join } from 'path';
+import { existsSync, mkdirSync, writeFileSync } from "fs"
+import { join } from "path"
 
 const name = process.argv[2]
-const newFolder = '../' + name
-if (!newFolder) throw new Error('include path name')
+const newFolder = "../" + name
+if (!newFolder) throw new Error("include path name")
 
 const folderExists = existsSync(join(__dirname, newFolder))
-if (folderExists) throw new Error('folder already exists')
+if (folderExists) throw new Error("folder already exists")
 
-const newFile = join(__dirname, newFolder, 'index.ts')
-const newInput = join(__dirname, newFolder, 'input.txt')
+const newFile = join(__dirname, newFolder, "index.ts")
+const newInput = join(__dirname, newFolder, "input.txt")
 
 mkdirSync(join(__dirname, newFolder))
-writeFileSync(newFile, `// Advent of Code: ${name}
+writeFileSync(
+  newFile,
+  `// Advent of Code: ${name}
 const ${name} = (input: string) => {
   const processed = processInput(input)
   
@@ -25,6 +27,6 @@ const processInput = (input: string): ProcessedInput => {
 type ProcessedInput = void
 
 export default ${name}
-`)
-writeFileSync(newInput, '')
-
+`,
+)
+writeFileSync(newInput, "")
--- day2_1/index.ts
+++ day2_1/index.ts
@@ -1,7 +1,7 @@
 // Advent of Code: day2_1
 const day2_1 = (input: string) => {
-  const commands = input.split('\n').map(x => x.split(' '))
-  let position: Command = [0,0]
+  const commands = input.split("\n").map((x) => x.split(" "))
+  let position: Command = [0, 0]
   for (let command of commands) {
     position = processDirection(position, command[0], command[1])
   }
@@ -13,11 +13,11 @@ type Command = [number, number]
 const processDirection = (pos: Command, direction: string, distance: string): Command => {
   const dist = Number(distance)
   switch (direction) {
-    case 'forward':
+    case "forward":
       return [pos[0] + dist, pos[1]]
-    case 'up':
+    case "up":
       return [pos[0], pos[1] - dist]
-    case 'down':
+    case "down":
       return [pos[0], pos[1] + dist]
     default:
       return pos
